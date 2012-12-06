40e87cf02b988e083243335fa5d8bf74fa665365
Matthew Martz <xiao_haozi@mutaku.com>
2012-12-06 16:24:02 -0500

    .

--- palindrome.cpp
+++ palindrome.cpp
@@ -119,12 +119,12 @@ int main(int argc, char * argv[]){
 
     // ------------------------------------------------------------
     // debugging things and making sure our conversions are working
-    cout << symmetry << " - symmetry" << endl;
-    cout << "start " << start << " - stop " << stop << endl;
-    long long test_int = 5;
-    string test_str = "5";
-    cout << typeid(symmetry).name() << typeid(test_int).name() << endl;
-    cout << typeid(symmetry_string).name() << typeid(test_str).name() << endl;
+    //cout << symmetry << " - symmetry" << endl;
+    //cout << "start " << start << " - stop " << stop << endl;
+    //long long test_int = 5;
+    //string test_str = "5";
+    //cout << typeid(symmetry).name() << typeid(test_int).name() << endl;
+    //cout << typeid(symmetry_string).name() << typeid(test_str).name() << endl;
     // end debugging stuffs 
     // ------------------------------------------------------------
     
